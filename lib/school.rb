9292011caccfb475314f4d078cc72c6fbc00596c
kylewstewart <kyle.w.stewart@mac.com>
2017-04-06 17:30:46 -0400

    Completed school-domain

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
-# code here!
+require 'pry'
+
+class School
+
+  attr_accessor :roster
+
+  def initialize(name)
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if @roster[grade] == nil
+      @roster[grade] = [name]
+    else
+      @roster[grade] << name
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    return_hash = {}
+    @roster.keys.sort.each do |grade|
+      sorted_array = []
+      sorted_array = @roster[grade].sort
+      return_hash[grade] =sorted_array
+    end
+    return_hash
+  end
+
+
+
+end
